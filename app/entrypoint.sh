c666d5ea38b31fb055de0fac711b5c8df3b74f65
Stanislav Drinkins <32555751+meawke@users.noreply.github.com>
2022-04-13 10:18:29 +0300

    + add configs

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+set +xe
+
+/usr/sbin/crond
+
 cd /app
 
-java -XX:+HeapDumpOnOutOfMemoryError -Dfile.encoding=UTF-8 -Dlogback.configurationFile=logback.xml -jar wowchat.jar wowchat.conf
+java -XX:+CrashOnOutOfMemoryError -Dfile.encoding=UTF-8 -Dlogback.configurationFile=logback.xml -jar wowchat.jar wowchat.conf
